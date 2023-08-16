e8be01fa69fd3380207b63e87bf8cf4c993f437b
자신의 <>
2023-08-16 22:44:18 +0900

    스킬1차완료

---
+++ src/assets/js/skill.js
@@ -0,0 +1,21 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+export function skill() {
+    gsap.registerPlugin(ScrollTrigger);
+    const horSection = gsap.utils.toArray(".skill__item");
+     gsap.to(horSection, {
+        xPercent: -120 * (horSection.length - 1),
+        ease: "none",
+        scrollTrigger: {
+            trigger: "#skill",
+            start: "top 56px",
+            end: "+=3000",
+            pin: true,
+            scrub: 1,
+            markers: false,
+            invalidateOnRefresh: true,
+            anticipatePin: 1,
+        },
+    });
+}
